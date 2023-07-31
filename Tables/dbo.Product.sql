acc12ab97c19dfc1add0f8529f36eb3079319701
Srini Gaddam <srinivasreddy759g@gmail.com>
2023-07-31 07:49:10 -0700

    Add database objects to repository

---
+++ Tables/dbo.Product.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Product] (
+  [Id] [int] IDENTITY,
+  [Name] [nvarchar](max) NULL,
+  [Description] [nvarchar](max) NULL,
+  [Price] [decimal](18, 2) NOT NULL,
+  CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([Id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
---
+++ Tables/dbo.UserData.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserData] (
+  [Id] [int] IDENTITY,
+  [Name] [varchar](60) NOT NULL,
+  [UserName] [varchar](30) NOT NULL,
+  [Password] [varchar](20) NOT NULL,
+  [Email] [varchar](50) NOT NULL,
+  CONSTRAINT [PK_UserData] PRIMARY KEY CLUSTERED ([Id])
+)
+ON [PRIMARY]
+GO
